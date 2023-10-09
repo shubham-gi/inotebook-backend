bc4b3c224f1cc4a15c8fa1f38c7d70af6e97cb2f
shubham-gi <121670989+shubham-gi@users.noreply.github.com>
2023-10-09 23:58:28 +0530

    video 45 completed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ connectToMongo()
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,12 @@
 const express=require('express')
+const User = require('../models/User')
 const router=express.Router()
-router.get('/',(req,res)=>{
-    res.json([])
+
+
+//create a user using post request
+router.post('/',(req,res)=>{
+    const user=new User(req.body)
+    user.save()
+    res.json("user saved")
 })
 module.exports=router
